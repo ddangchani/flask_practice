88c8939fa1db70dbccdd46b7c7687f2cc00ddac9
Dangchan <dang11230@gmail.com>
2023-04-08 19:34:04 +0900

    CH2 clear

---
+++ pybo/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate() # 여기서 생성해주어야 블루프린트 등 다른 모듈에서 사용가능!
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db) # create_app 외부에서 생성했으므로 객체의 앱 등록 과정이 필요
+    from . import models
+    
+    # Blueprint
+    from .views import main_views, question_views, answer_views
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
+
+    return app
